96250b43799d839b68a23421f7d21f6c55903fb0
kurotamashi30 <kurotamashi30@gmail.com>
2024-08-30 23:17:33 +0900

    not fond page

--- src/layout/AppLayout.js
+++ src/layout/AppLayout.js
@@ -2,32 +2,34 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import handImage from './hand_700px.png';
+//import handImage from './hand_700px.png';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { Outlet } from 'react-router-dom';
+import './AppLayout.style.css'
 
 
 const AppLayout = () => {
   return (
-    <div>
+    <div className='Navbar_full'>
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
+          <div className='loge_img'>
         <img
           alt="Hand Icon"
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Netflix_icon.svg/2048px-Netflix_icon.svg.png"
           width="50"
           height="50"
-          margin_right="5px"
           className="d-inline-block align-top"
         />
+        </div>
           <Navbar.Brand href="/">Movie_app</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="/" >Homepage</Nav.Link>
             <Nav.Link href="/movies">Movies</Nav.Link>
-            <Nav.Link href="/movies/:id">MoviesDtail</Nav.Link>
+            <Nav.Link href="/movies/:id">MoviesDetail</Nav.Link>
           </Nav>
           <Form inline>
         <Row>
@@ -39,7 +41,7 @@ const AppLayout = () => {
             />
           </Col>
           <Col xs="auto">
-            <Button variant="danger">Submit</Button>
+            <Button variant="outline-danger">Submit</Button>
           </Col>
         </Row>
       </Form>
--- src/pages/Notfound/Notfoundpage.js
+++ src/pages/Notfound/Notfoundpage.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Notfoundpage.style.css';
 
 const Notfoundpage = () => {
   return (
-    <div>
-      Notfoundpage
+    <div className="not-found_container">
+      <div className="not-found">
+        <h1>SORRY 😿</h1>
+        <h3>we could't find that page</h3>
+        <Link to="/">
+          <button>Go to Main Page</button>
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Notfoundpage
+export default Notfoundpage;
