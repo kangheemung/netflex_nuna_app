7ebf2d2655fff7df194ebc3a2a13286a19910856
kurotamashi30 <kurotamashi30@gmail.com>
2024-09-02 13:08:08 +0900

    test

--- src/pages/Homepage/components/Banner/Banner.js
+++ src/pages/Homepage/components/Banner/Banner.js
@@ -14,14 +14,18 @@ const Banner = () => {
     }
   
     return (
+      <div className='banner_area'>
       <div style={{
         backgroundImage: "url("+`https://www.themoviedb.org/t/p/w600_and_h900_bestv2${data.results[0].backdrop_path}`+")"
       }} className='banner'>
         <div className='banner-text-area'>
+          <div className='banner-text'>
           <h1 className='title'>{data.results[0].title}</h1>
           <p>{data.results[0].overview}</p>
+          </div>
         </div>
 
+      </div>
     </div>
   );
 }
